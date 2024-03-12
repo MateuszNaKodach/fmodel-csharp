686dcdf0e55471d0c5666beb12152a2275e5854e
Mateusz Nowak <mateusz.nowak@justjoin.it>
2024-03-12 01:03:11 +0100

    decider nullable fix

--- src/Fraktalio.FModel.Domain/Decider.cs
+++ src/Fraktalio.FModel.Domain/Decider.cs
@@ -6,11 +6,6 @@ namespace Fraktalio.FModel.Domain;
  * [Decider] can be specialized for any type `C` or `S` or `E` because these types does not affect its behavior.
  * [Decider] behaves the same for `C`=[Int] or `C`=`OddNumberCommand`.
  *
- * If you want to know more about this pattern please check out following resources:
- * - https://event-driven.io/en/how_to_effectively_compose_your_business_logic/ (How to effectively compose your business logic)
- * - https://github.com/fraktalio/fmodel (implementation with examples on whole domains in Kotlin)
- * - https://bettersoftwaredesign.pl/episodes/50 (Better Software Design - 50. O implementacji logiki biznesowej z Decider Pattern z Oskarem Dudyczem)
- *
  * @param C Command
  * @param S State
  * @param E Event
@@ -25,21 +20,21 @@ public interface IDecider<in TCommand, TState, TEvent>
 {
     IEnumerable<TEvent> Decide(TCommand c, TState? s);
 
-    TState? Evolve(TState? s, TEvent e);
+    TState Evolve(TState s, TEvent e);
 
-    TState? InitialState { get; }
+    TState InitialState { get; }
 }
 
 public record Decider<TCommand, TState, TEvent>(
-    Func<TCommand, TState?, IEnumerable<TEvent>> Decide,
-    Func<TState?, TEvent, TState?> Evolve,
-    TState? InitialState
+    Func<TCommand, TState, IEnumerable<TEvent>> Decide,
+    Func<TState, TEvent, TState> Evolve,
+    TState InitialState
 ) : IDecider<TCommand, TState, TEvent>
     where TCommand : class
     where TState : class?
     where TEvent : class
 {
-    IEnumerable<TEvent> IDecider<TCommand, TState, TEvent>.Decide(TCommand c, TState? s) => Decide.Invoke(c, s);
+    IEnumerable<TEvent> IDecider<TCommand, TState, TEvent>.Decide(TCommand c, TState s) => Decide.Invoke(c, s);
 
-    TState? IDecider<TCommand, TState, TEvent>.Evolve(TState? s, TEvent e) => Evolve.Invoke(s, e);
+    TState IDecider<TCommand, TState, TEvent>.Evolve(TState s, TEvent e) => Evolve.Invoke(s, e);
 }
